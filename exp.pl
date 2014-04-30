d7bcdb846ba2cc80d92e4772276526992a9a2b01
Dave Tang <me@davetang.org>
2014-04-30 23:26:24 +0900

    Added comments to explain code

--- exp.pl
+++ exp.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/bin/env perl
 
 use strict;
 use warnings;
@@ -6,12 +6,15 @@ use warnings;
 my $usage = "Usage: $0 <infile.bam>\n";
 my $infile = shift or die $usage;
 
+#for storing results
 my %data = ();
 
 open(IN,'-|',"samtools view $infile") || die "Could not open $infile: $!\n";
 while(<IN>){
    chomp;
-   my ($qname,$flag,$rname,$pos,$mapq,$cigar,$mrnm,$mpos,$isize,$seq,$qual,$tag,$vtype,$value) = split;
+   my ($qname,$flag,$rname,$pos,$mapq,$cigar,$mrnm,$mpos,$isize,$seq,$qual,$tag,$vtype,$value) = split(/\t/);
+	#hash key is the name of read
+	#if read maps to another piRNA increment
    if (exists $data{$qname}){
       my $number = scalar(@{$data{$qname}});
       $data{$qname}->[$number] = $rname;
@@ -21,18 +24,24 @@ while(<IN>){
 }
 close(IN);
 
+#for storing expression of piRNAs
 my %exp = ();
 
 foreach my $qname (keys %data){
    my $multimap = scalar(@{$data{$qname}});
+	#don't work with reads mapping to 5 or more piRNAs
    next if $multimap > 5;
-   #print "$qname\t$multimap\n";
-   foreach my $number (@{$data{$qname}}){
-      #print "\t$number\n";
-      if(exists $exp{$number}){
-         $exp{$number} += 1 / $multimap;
+	#print "$qname\t$multimap\n";
+	#go through all piRNAs that the read mapped to
+   foreach my $pirna (@{$data{$qname}}){
+		#print "\t$pirna\n";
+		#tally expression for the piRNA
+		#and divide by the number of times
+		#the read multimapped
+      if(exists $exp{$pirna}){
+         $exp{$pirna} += 1 / $multimap;
       } else {
-         $exp{$number} = 1 / $multimap;
+         $exp{$pirna} = 1 / $multimap;
       }
    }
 }
