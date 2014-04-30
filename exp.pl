16eb9d49d0efa5599118f00ffbefe0bf91479437
Dave Tang <me@davetang.org>
2014-04-30 23:30:58 +0900

    Round expression

--- exp.pl
+++ exp.pl
@@ -13,8 +13,8 @@ open(IN,'-|',"samtools view $infile") || die "Could not open $infile: $!\n";
 while(<IN>){
    chomp;
    my ($qname,$flag,$rname,$pos,$mapq,$cigar,$mrnm,$mpos,$isize,$seq,$qual,$tag,$vtype,$value) = split(/\t/);
-	#hash key is the name of read
-	#if read maps to another piRNA increment
+   #hash key is the name of read
+   #if read maps to another piRNA increment
    if (exists $data{$qname}){
       my $number = scalar(@{$data{$qname}});
       $data{$qname}->[$number] = $rname;
@@ -29,15 +29,15 @@ my %exp = ();
 
 foreach my $qname (keys %data){
    my $multimap = scalar(@{$data{$qname}});
-	#don't work with reads mapping to 5 or more piRNAs
+   #don't work with reads mapping to 5 or more piRNAs
    next if $multimap > 5;
-	#print "$qname\t$multimap\n";
-	#go through all piRNAs that the read mapped to
+   #print "$qname\t$multimap\n";
+   #go through all piRNAs that the read mapped to
    foreach my $pirna (@{$data{$qname}}){
-		#print "\t$pirna\n";
-		#tally expression for the piRNA
-		#and divide by the number of times
-		#the read multimapped
+      #print "\t$pirna\n";
+      #tally expression for the piRNA
+      #and divide by the number of times
+      #the read multimapped
       if(exists $exp{$pirna}){
          $exp{$pirna} += 1 / $multimap;
       } else {
@@ -47,7 +47,8 @@ foreach my $qname (keys %data){
 }
 
 foreach my $pirna (sort {$exp{$b} <=> $exp{$a}} keys %exp){
-   print "$pirna\t$exp{$pirna}\n";
+   my $pirna_exp = sprintf("%.2f", $exp{$pirna});
+   print "$pirna\t$pirna_exp\n";
 }
 
 exit(0);
