c96e9d871affed34ed60053b8e291a4484b07e95
Dave Tang <davetingpongtang@gmail.com>
2014-05-01 21:46:41 +0900

    Map with 8 cores

--- map.sh
+++ map.sh
@@ -6,7 +6,7 @@ for file in `ls *.csfasta`;
    do base=`basename $file .csfasta`;
    echo "Mapping $file"
    #default settings
-   SHRiMP_2_2_3/bin/gmapper-cs $file $database -N 12 > ${base}_mouse_pirna.sam 2> ${base}_mouse_pirna.log
+   SHRiMP_2_2_3/bin/gmapper-cs $file $database -N 8 > ${base}_mouse_pirna.sam 2> ${base}_mouse_pirna.log
    samtools view -bS ${base}_mouse_pirna.sam > ${base}_mouse_pirna.bam;
    samtools sort ${base}_mouse_pirna.bam ${base}_mouse_pirna_sorted;
    rm ${base}_mouse_pirna.sam ${base}_mouse_pirna.bam;
